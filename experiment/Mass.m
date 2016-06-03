484943f2ab380b6f1932736bd7c7f609955f49eb
Jason <jason@crabba.de>
2016-06-03 20:47:39 +0200

    Falcon 9 1.1 Startwerte. Luftdruckänderung nicht berücksichtigt. Aufstieg etwas zu langsam

--- experiment/Mass.m
+++ experiment/Mass.m
@@ -3,6 +3,6 @@ function [ mass ] = Mass( bruttoWeight, tankLevel, tankVolume )
     % tankLevel is percentage of tankVolume filled (0.0-1.0)
     % tankVolume is the maximum weight of the filled tank in tonnes
     
-    mass = bruttoWeight + tankLevel * tankVolume;
+    mass = 1000 * (bruttoWeight + tankLevel * tankVolume);
 end
 
