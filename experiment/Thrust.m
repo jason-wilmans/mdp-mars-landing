9efeb994dfb50b4b94e9ccabc1466151ca6430a3
Jason <jason@crabba.de>
2016-07-04 20:57:14 +0200

    mnbvc

--- experiment/Thrust.m
+++ experiment/Thrust.m
@@ -1,4 +1,4 @@
 function [ thrust ] = Thrust( throttle, rocketPower, mass )
-    %rocketPower max thrust in kN
+    %rocketPower = max thrust in kN
     thrust = (throttle * rocketPower * 1000) / mass;
 end
